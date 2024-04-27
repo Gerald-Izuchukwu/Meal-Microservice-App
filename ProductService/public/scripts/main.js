2438615ef9adbd816ad23996d713ecf8b6a5811b
gerald-izuchukwu <geraldlouisugwunna@gmail.com>
2024-04-27 11:26:50 +0100

    lil change

--- ProductService/public/scripts/main.js
+++ ProductService/public/scripts/main.js
@@ -1,58 +1,27 @@
-// const axios = require('axios').default
 document.addEventListener("DOMContentLoaded", function () {
-    const firstNameInput = document.getElementById("firstName");
-    const lastNameInput = document.getElementById("lastName");
-    const emailInput = document.getElementById("email");
-    const newPasswordInput = document.getElementById("password");
-    const submitButton = document.getElementById("submit-button");
-    const flashMessage = document.getElementById('flash-message');
+    const selectButton = document.getElementById("select-button");
+    const selectedFood = document.getElementById('foods')
+    const foodList = document.getElementsByClassName('food-list')
+    const buyFoodButton = document.getElementsByClassName('buy-food')
+
+    selectButton.addEventListener('click', async function (e){
+        e.preventDefault()
+        // buyFoodButton.style.display = 'block'
+
+        console.log(selectedFood.value)
+        const url = `http://localhost:9601/meal-api/v1/food/get-food-type?type=${selectedFood.value}`
+        console.log('yes');
+        // const url = `http://localhost:9601/meal-api/v1/food/get-food-type?type=soups`
+
+        const response = await axios.get(url)
+        console.log(response)
+        foodList.style.display = "block"
+        buyFoodButton.childElement.innerHTML = "Buy Selected Food"
+
+    })
 
 
-    submitButton.addEventListener("click", async function (event) {
-        event.preventDefault()
-        const url = 'http://localhost:9602/meal-api/v1/auth/register'
-        const firstName = firstNameInput.value;
-        const lastName = lastNameInput.value;
-        const email = emailInput.value;
-        const newPassword = newPasswordInput.value;
-        const name = `${firstName} ${lastName}`
-        const details ={
-            name,
-            email, 
-            password: newPassword
-        }
-        function showFlashMessage(message, color) {
-            flashMessage.textContent = message;
-            flashMessage.style.display = 'block';
-            flashMessage.style.backgroundColor = color
-            setTimeout(() => {
-                flashMessage.style.display = 'none';
-            }, 3000); // Display the message for 3 seconds (adjust as needed)
-        }
 
-
-        try {
-            const response = await axios.post(url, details)
-            const message = response.data
-            showFlashMessage(message, 'green')
-            console.log(response)
-            console.log(message)
-            if (response.status === 200 && response.headers.location) {
-                window.location.href = response.headers.location;
-            }
-        } catch (error) {
-            if(error){
-                console.log(error)
-                console.error('Response status:', error.response.status);
-                console.error('Response data:', error.response.data);
-                showFlashMessage(error.response.data)
-            }else if (error.request) {
-                console.error('No response received:', error.request);
-            } else {
-                console.error('Request error:', error.message);
-            }
-        }
-    });
 })
 
         // function clearFeilds() {
--- ProductService/routes/foodRoutes.js
+++ ProductService/routes/foodRoutes.js
@@ -17,7 +17,7 @@ const {foodPage, drinkPage} = require('../pages/productPages')
 
 router.route('/buy-food').post(isAuthenticated, buyFood)
 router.route('/get-food').get( getFood)
-router.route('/get-food-page').get(foodPage)
+router.route('/home-page').get(foodPage)
 router.route('/get-food-type').get( getFoodBasedOnType)
 router.route('/add-food').post(isAuthenticated, addFood)
 router.route('/discountedFoods').get(getDiscountedFood)
