95ed44382c91449cf138bd3ebc79b18f36f5ea8b
Gerald-Izuchukwu <geraldlouisugwunna@gmail.com>
2026-06-30 23:14:06 +0100

    modified:   DeliveryService/isAuthenticated.js         modified:   OrderService/isAuthenticated.js         modified:   ProductService/isAuthenticated.js

--- ProductService/isAuthenticated.js
+++ ProductService/isAuthenticated.js
@@ -4,7 +4,6 @@ async function isAuthenticated(req, res, next){
     // creating header to be "Bearer <token>", if its splited using space " ", then it becomes an array of two elem - Bearer and token
     const token = req.headers["authorization"].split(" ")[1]
 
-    // verify the token to know if its legit
     jwt.verify(token, secret, (err, user)=>{
         if (err){
             console.log(err);
